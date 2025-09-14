5b163b5b213889af61ad4d2d50985d5c9d9c7636
Thanh Bao <thanhbaomail@gmail.com>
2025-09-14 17:27:12 +0700

    add them web

---
+++ TraSuaApp.Infrastructure/Migrations/20250913090123_AddEntityIdTd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TraSuaApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntityIdTd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimKiem",
+                table: "KhachHangs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimKiem",
+                table: "KhachHangs");
+        }
+    }
+}
